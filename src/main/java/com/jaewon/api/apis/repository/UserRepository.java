42dca1647667a9b34e2e424250c25519df94cf6a
noling0321 <52159352+noling0321@users.noreply.github.com>
2020-12-14 14:11:54 +0900

    add: UserRepository

---
+++ src/main/java/com/jaewon/api/apis/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.jaewon.api.apis.repository;
+
+import com.jaewon.api.apis.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
