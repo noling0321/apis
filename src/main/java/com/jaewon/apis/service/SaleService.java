67f6b80fd4787b8f122d68eafbab87cf90d42fa8
noling0321 <52159352+noling0321@users.noreply.github.com>
2020-12-16 08:49:45 +0900

    Unversioned files

---
+++ src/main/java/com/jaewon/apis/service/SaleService.java
@@ -0,0 +1,55 @@
+package com.jaewon.apis.service;
+
+import com.jaewon.apis.model.Product;
+import com.jaewon.apis.model.Sale;
+import com.jaewon.apis.repository.SaleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import java.util.Optional;
+
+@Controller
+public class SaleService {
+    private final SaleRepository saleRepository;
+
+    @Autowired
+    public SaleService(SaleRepository saleRepository) {
+        this.saleRepository = saleRepository;
+    }
+
+    public Sale find(int saleId) throws Exception {
+        Optional<Sale> searchedSale = this.saleRepository.findById(saleId);
+        return searchedSale.orElseThrow(() -> new Exception("해당 상품을 찾지 못하였습니다."));
+    }
+
+    public void initializeSales() {
+        Sale sale1 = Sale.builder()
+                .userId(1)
+                .productId(1)
+                .listPrice(1200000)
+                .paidPrice(1000000)
+                .amount(1)
+                .build();
+
+        Sale sale2 = Sale.builder()
+                .userId(2)
+                .productId(2)
+                .listPrice(1240000 * 2)
+                .paidPrice(1110000 * 2)
+                .amount(2)
+                .build();
+
+        Sale sale3 = Sale.builder()
+                .userId(3)
+                .productId(3)
+                .listPrice(230000 * 3)
+                .paidPrice(210000 * 3)
+                .amount(3)
+                .build();
+
+        this.saleRepository.save(sale1);
+        this.saleRepository.save(sale2);
+        this.saleRepository.save(sale3);
+        this.saleRepository.flush();
+    }
+}
---
+++ src/main/java/com/jaewon/apis/service/UserService.java
@@ -0,0 +1,69 @@
+package com.jaewon.apis.service;
+
+import com.jaewon.apis.model.User;
+import com.jaewon.apis.repository.UserRepository;
+import com.jaewon.apis.vo.UserRegisterVO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+public class UserService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User find(int userId) throws Exception {
+        Optional<User> searchedUser = this.userRepository.findById(userId);
+        return searchedUser.orElseThrow(() -> new Exception("해당 유저를 찾지 못하였습니다."));
+    }
+
+    public List findAll() {
+        return this.userRepository.findAll();
+    }
+
+    public void initializeUsers() {
+        User user1 = User.builder()
+                .email("example1@sample.com")
+                .name("Mr. Example")
+                .phone("01000000000")
+                .build();
+
+        User user2 = User.builder()
+                .email("example2@sample.com")
+                .name("Mrs. Sample")
+                .phone("01000000000")
+                .build();
+
+        User user3 = User.builder()
+                .email("example3@sample.com")
+                .name("ms. Sample Data")
+                .phone("01000000000")
+                .build();
+
+        this.userRepository.save(user1);
+        this.userRepository.save(user2);
+        this.userRepository.save(user3);
+        this.userRepository.flush();
+    }
+
+    public void createUser(UserRegisterVO userRegisterVO) {
+        User createUser = User.builder()
+                .email(userRegisterVO.getEmail())
+                .phone(userRegisterVO.getPhone())
+                .name(userRegisterVO.getName())
+                .build();
+
+        this.userRepository.save(createUser);
+        this.userRepository.flush();
+    }
+
+    public void deleteUser(int userId) {
+        this.userRepository.deleteById(userId);
+    }
+}
